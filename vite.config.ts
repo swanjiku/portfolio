2d583c627f29ac1a9954783f1946e78a0336b3e8
dev <>
2024-11-06 11:48:32 +0300

    base changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/portfolio/', // Must match your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
